7b9c0eaabdcc4a7f0dd658e6a81bca6f1241f55f
Swapnil Mali <maliswapnil982@gmail.com>
2019-05-21 15:54:14 +0530

    Added run on startup feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-https://www.youtube.com/watch?v=sQFCe_W14js
+Inspired by:  https://www.youtube.com/watch?v=sQFCe_W14js
 """
 from bs4 import BeautifulSoup
 import requests
@@ -9,16 +9,30 @@ import time
 from PIL import Image, ImageDraw, ImageFont
 import ctypes
 import os
+import shutil
 
+# move shortcut to main.exe to startup folder
+try:
+    # get user name
+    user = os.getlogin()
+    path = r'C:\Users\{}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\main - Shortcut.lnk'.format(user)
+    # print(path)
+    shutil.move(r'main - Shortcut.lnk', path)
+except FileNotFoundError:
+    pass
 
+# just credit and copyright stuff
 print('@author: Swapnil Mali \n\n(Note: New wallpaper is available everyday after 2.00 pm)')
 print("Downloading Today's Wallpaper...please wait!!")
+
+# get image link from the website page
 res = requests.get('https://bing.wallpaper.pics/')
 soup = BeautifulSoup(res.text, 'lxml')
 image_box = soup.find('a', {'class': 'cursor_zoom'})
 image = image_box.find('img')
 link = image['src']
 
+# download and save the image
 filename = datetime.now().strftime('%d-%m-%y')
 urllib.request.urlretrieve(link, '{}.jpg'.format(filename))
 
@@ -26,20 +40,19 @@ urllib.request.urlretrieve(link, '{}.jpg'.format(filename))
 image = Image.open('{}.jpg'.format(filename))
 font_type = ImageFont.truetype('fonts/Quicksand-Bold.otf', 44)
 draw = ImageDraw.Draw(image)
-draw.text(xy=(800, 1000), text='© Swapnil Mali', fill=(0,0,0), font=font_type)
+draw.text(xy=(800, 1000), text='© Swapnil Mali', fill=(0, 0, 0), font=font_type)
 # image.show()
 image.save('{}.jpg'.format(filename))
 
 print("\n\n-------------------------------------------\nDone..New wallpaper saved as '{}.jpg'\n-------------------------------------------".format(filename))
 time.sleep(1)
+
 # set new image as desktop background
 directory = os.getcwd()
-# print(directory)
 image_path = '{}\{}.jpg'.format(directory, filename)
-ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
-
 print("\nSetting new Wallpaper..".format(filename))
+ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
 time.sleep(2)
-print("Done..Closing this window")
 
+print("Done..Closing this window")
 time.sleep(2)
